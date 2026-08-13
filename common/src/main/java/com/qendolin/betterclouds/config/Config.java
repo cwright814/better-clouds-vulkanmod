6a65bd3c46f59bf278d9ccf98ffa111bd2dd7349
Christopher Wright <cwright814@gmail.com>
2026-08-12 22:23:08 -0400

    Revert travel speed to 0.1f and slider max to 0.4f

--- common/src/main/java/com/qendolin/betterclouds/config/Config.java
+++ common/src/main/java/com/qendolin/betterclouds/config/Config.java
@@ -58,7 +58,7 @@ public class Config {
     @SerialEntry
     public float sizeY = 12f;
     @SerialEntry
-    public float travelSpeed = 2.0f;
+    public float travelSpeed = 0.1f;
     @SerialEntry
     public float windEffectFactor = 1.0f;
     @SerialEntry
