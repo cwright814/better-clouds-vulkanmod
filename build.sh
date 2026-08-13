d428015720b4a3a31970a7438ca1d1ca86c4b718
Christopher Wright <cwright814@gmail.com>
2026-08-12 20:41:53 -0400

    Add development environment reference comment to build script

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# This script is hard-coded for my personal development environment. Providing it on GitHub solely as reference for how builds were generated. You will need to set up your own build workflow. An LLM can likely adapt this file for you if you are not sure where to begin.
+# -Christopher
+
 echo "Building fabric mod..."
 JAVA_HOME=~/.jdks/graalvm-25.1.3+9.1 GRADLE_OPTS="--enable-native-access=ALL-UNNAMED" ./gradlew clean :fabric:build
 
