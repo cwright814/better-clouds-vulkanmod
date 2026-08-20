41c3959f353f6fd5b521ca569904bb94630d9156
Christopher Wright <cwright814@gmail.com>
2026-08-20 14:07:06 -0400

    Bump to 1.16.0

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ set -e
 echo "Building fabric mod..."
 JAVA_HOME=~/.jdks/graalvm-25.1.3+9.1 GRADLE_OPTS="--enable-native-access=ALL-UNNAMED" ./gradlew clean :fabric:build
 
-echo "Copying to sandbox directory..."
+echo "Copying to parent directory..."
 MOD_VERSION=$(grep "^mod\.version=" gradle.properties | cut -d'=' -f2)
 
 # Use find to locate the exact jar (ignoring -sources.jar) and copy it
-find fabric/build/libs -name "better-clouds-*.jar" ! -name "*-sources.jar" -exec cp {} /home/cwright/Projects/sandbox/better-clouds-vulkanmod-${MOD_VERSION}+26.1.2-fabric.jar \;
+find fabric/build/libs -name "better-clouds-*.jar" ! -name "*-sources.jar" -exec cp {} ../better-clouds-vulkanmod-${MOD_VERSION}+26.1.2-fabric.jar \;
 
 echo "Build and copy complete!"
